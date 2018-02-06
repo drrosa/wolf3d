493182555500ff825ec5262df72e0f1ba38654de
David Rosa Tamsen <rosatamsen@gmail.com>
2018-02-06 00:17:15 -0800

    SDL: Basic open new window and wait until user quits.

--- Makefile
+++ Makefile
@@ -1,3 +1,20 @@
 all:
-	gcc wolf3d.c -I SDL2/ -L SDL2/ -lSDL2 -o wolf3d
+
+tutorial:
+	gcc tutorial.c -I SDL2/ -L SDL2/ -lSDL2 -o tutorial
+
+example:
+	g++ example.c instantcg.cpp -L ~/.brew/lib/ -lSDL2 -lSDL2main -I. -I ~/.brew/include/ -o example
+
+rayflat:
+	g++ raycaster_flat.cpp -L ~/.brew/lib/ -lSDL2 -lSDL2main -I. -I ~/.brew/include/ instantcg.cpp -o rayflat
+
+wolf3d:	
+	gcc wolf3d.c -L ~/.brew/lib/ -lSDL2 -lSDL2main -I. -I ~/.brew/include/ -o wolf3d
+
+re: clean wolf3d
+	./wolf3d
+
+clean:
+	rm wolf3d
 #set -x DYLD_LIBRARY_PATH $PWD/libs/ $DYLD_LIBRARY_PATH
--- wolf3d.c
+++ wolf3d.c
@@ -15,33 +15,77 @@
 
 int main(int argc, char *args[])
 {
+	SDL_Window *window;
+	SDL_Renderer* renderer;
+	window = init_window(false, "Wolf3D");
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL)
+		put_error("Renderer could not be created! SDL_Error: ");
+	SDL_RenderPresent(renderer);
+	sleep();
+	end(renderer, window);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
+
+SDL_Window *init_window(bool fullscreen, const char *title)
+{
 	SDL_Window *window = NULL;
 	SDL_Surface *screenSurface = NULL;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		put_error("SDL could not be initialized! SDL_Error: ");
 	}
 	else
 	{
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
-								  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+		window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED,
+								  SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,
 								  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n",
-				   SDL_GetError());
-		}
-		else
-		{
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(screenSurface, NULL,
-						 SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
-			SDL_Delay(2000);
-		}
+			put_error("Window could not be created! SDL_Error: ");
+		// else
+		// {
+		// 	screenSurface = SDL_GetWindowSurface(window);
+		// 	SDL_FillRect(screenSurface, NULL,
+		// 				 SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+		// 	SDL_UpdateWindowSurface(window);
+		// 	SDL_Delay(2000);
+		// }
 	}
+	return (window);
+}
+
+void put_error(char *str)
+{
+	perror(str);
+	perror(SDL_GetError());
+	perror("\n");
+	SDL_Quit();
+	exit(-1);
+}
+
+// pauses the program until you press a key
+void sleep()
+{
+	SDL_Event event = {0};
+  bool done = false;
+  while(done == false)
+  {
+    while(SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_QUIT) return ;
+      if (event.type == SDL_KEYDOWN) done = true;
+    }
+    SDL_Delay(5);
+  }
+}
+
+void end(SDL_Renderer *renderer, SDL_Window *window)
+{
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-	return 0;
+	exit(1);
 }
